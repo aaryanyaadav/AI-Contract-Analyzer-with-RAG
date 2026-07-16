c7f942f053c9e1f2e9934a6b84075fa28a805ab8
Aryan Yadav <aryanyadav051206@gmail.com>
2026-07-16 23:28:59 +0530

    response validator updated

--- orchestration/response_validator.py
+++ orchestration/response_validator.py
@@ -1,79 +1,44 @@
 class ResponseValidator:
-
-
     def validate(
-
         self,
-
         answer,
-
         retrieved_chunks=None
     ):
-
         if not answer:
-
             return False
-
         if len(answer.strip()) < 10:
-
             return False
-
-
         bad_phrases = [
-
             "i don't know",
-
             "no information available",
-
             "cannot determine",
-
             "not enough context"
         ]
-
         answer_lower = answer.lower()
-
         if any(
-
             phrase in answer_lower
-
             for phrase in bad_phrases
         ):
-
             return False
-
-
         if retrieved_chunks:
-
             combined_context = " ".join([
-
                 chunk["text"].lower()
-
                 for chunk in retrieved_chunks
             ])
-
             # VERY LIGHT VALIDATION
             # (can improve later)
             important_terms = [
-
                 word.lower()
-
                 for word in answer.split()
-
                 if len(word) > 6
             ]
-
             overlap = any(
-
                 term in combined_context
-
                 for term in important_terms
             )
-
             if not overlap:
-
                 print(
                     "[Validator] "
                     "Low context overlap."
                 )
-
         return True
