f873030d7f3e94c80954952fc977305987e41ad5
Aryan Yadav <aryanyadav051206@gmail.com>
2026-07-16 23:27:01 +0530

    context builder updated

--- orchestration/context_builder.py
+++ orchestration/context_builder.py
@@ -1,25 +1,18 @@
 class ContextBuilder:
-
     def build_context(
         self,
         retrieved_chunks
     ):
-
         context_parts = []
-
         for idx, chunk in enumerate(retrieved_chunks):
-
             section = chunk["metadata"].get(
                 "parent_section",
                 "Unknown"
             )
-
             context_parts.append(
                 f"""
 [Section: {section}]
-
 {chunk['text']}
 """
             )
-
         return "\n".join(context_parts)
