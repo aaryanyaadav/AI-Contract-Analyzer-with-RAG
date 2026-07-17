e69c18c28084a047a6e8ce85139025159b339731
Aryan Yadav <aryanyadav051206@gmail.com>
2026-07-17 15:26:17 +0530

    doc serializer updated

--- ingestion/document_serializer.py
+++ ingestion/document_serializer.py
@@ -1,38 +1,21 @@
 
 import json
-
-
 class DocumentSerializer:
-
     def serialize_document(
-
         self,
-
         document_id,
-
         filename,
-
         metadata,
-
         chunks
     ):
-
         final_document = {
-
-
             "document_id": document_id,
-
             "filename": filename,
-
             "metadata": metadata,
-
             "chunks": chunks
         }
-
         return json.dumps(
-
             final_document,
-
             indent=2
         )
 
